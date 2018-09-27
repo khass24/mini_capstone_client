13933b83e0a284cf4254ad23eaf50e1c2680f564
Kyle Hassett <kylehassett72@gmail.com>
2018-09-26 21:23:55 -0500

    first commit

---
+++ app/controllers/client/products_controller.rb
@@ -0,0 +1,7 @@
+class Client::ProductsController < ApplicationController
+  def index
+    response = HTTP.get("http://localhost:3000/api/products")
+    @products = response.parse
+    render 'index.html.erb'
+  end
+end
